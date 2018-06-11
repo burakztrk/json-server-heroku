3e1cc748309058daa35b6962433c4d443051ade9
bozturk <bozturk@corbone.com.tr>
2018-06-11 17:08:20 +0300

    added second db

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const router2 = jsonServer.router('db2.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.use(router2);
 
 server.listen(port);
